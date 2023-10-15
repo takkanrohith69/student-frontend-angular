118d47cc80eaf457fe336e83d99f01213e3b4521
takkan rohith <takkanrohith69@gmail.com>
2023-10-15 10:03:04 +0530

    student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+ constructor(private http:HttpClient){}
+ onSubmit(data:any){
+  this.http.post("http://localhost:8080/student",data)
+  .subscribe((result) =>{
+    console.warn("result",result)
+  })
+    console.warn(data);
+ }
 }
